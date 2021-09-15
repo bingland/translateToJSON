fdc04a767e84ea8888b4f36ed3f38965d5e4e870
Brayden England <bray.patriot@gmail.com>
2021-09-15 13:49:57 -0600

    Output all languages as one object

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 (async () => {
-  const browser = await puppeteer.launch({headless: true, dumpio: true, args: ['--no-sandbox']});
+  const browser = await puppeteer.launch({headless: true, dumpio: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const langCodes = ['es', 'pt', 'fr', 'pl', 'de', 'da', 'sv', 'et']
   let jsonString = `{
     "firstThing": "Hello world!",
@@ -12,25 +12,23 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
   let requestText = Object.values(JSON.parse(jsonString)).join('%0A').replace(/ /g,'%20')
   const page = await browser.newPage();
 
+  let response = []
   for (let i in langCodes) {
-    console.log('Language: ' + langCodes[i])
-
+    console.log('[Server] Currently reading: ' + langCodes[i])
     await page.goto(`https://translate.google.com/?sl=en&tl=${langCodes[i]}&text=${requestText}&op=translate`, {waitUntil: 'networkidle2'});
-  
     await page.waitForSelector('.VIiyi')
     let data = await page.evaluate(() => {
       let translationData = document.querySelector('.VIiyi').innerText.split('\n')
       return translationData
     })
-    console.log(data)
-    let output = {}
-    Object.keys(JSON.parse(jsonString)).forEach((key, i) => {
-      output[key] = data[i]
-    })
-    console.log(output)
-    await delay(1000)
+    let currentOutput = {}
+    Object.keys(JSON.parse(jsonString)).forEach((key, i) => { currentOutput[key] = data[i] })
+    response.push({lang: langCodes[i], translations: currentOutput})
+    // console.log(currentOutput)
+    await delay(1500)
   }
 
-  // await page.screenshot({ path: './screenshots/example.png' }); 
+  console.log(response)
+
   await browser.close(); 
 })();
