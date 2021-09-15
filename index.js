dbd0feff97746be1faf86a1872073db12b455c1c
Brayden England <bray.patriot@gmail.com>
2021-09-15 13:26:39 -0600

    Request all langauges in array through loop

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const puppeteer = require('puppeteer');
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 (async () => {
-  const browser = await puppeteer.launch({headless: false, dumpio: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({headless: true, dumpio: true, args: ['--no-sandbox']});
+  const langCodes = ['es', 'pt', 'fr', 'pl', 'de', 'da', 'sv', 'et']
   let jsonString = `{
     "firstThing": "Hello world!",
     "secondThing": "I am translating!"
   }`
   let requestText = Object.values(JSON.parse(jsonString)).join('%0A').replace(/ /g,'%20')
-  console.log(requestText)
   const page = await browser.newPage();
-  await page.goto(`https://translate.google.com/?sl=en&tl=fr&text=${requestText}&op=translate`, {waitUntil: 'networkidle2'});
+
+  for (let i in langCodes) {
+    console.log('Language: ' + langCodes[i])
+
+    await page.goto(`https://translate.google.com/?sl=en&tl=${langCodes[i]}&text=${requestText}&op=translate`, {waitUntil: 'networkidle2'});
   
-  await page.waitForSelector('.VIiyi')
-  let data = await page.evaluate(() => {
-    let translationData = document.querySelector('.VIiyi').innerText.split('\n')
-    return translationData
-  })
-  console.log(data)
-  let output = {}
-  Object.keys(JSON.parse(jsonString)).forEach((key, i) => {
-    output[key] = data[i]
-  })
-  console.log(output)
+    await page.waitForSelector('.VIiyi')
+    let data = await page.evaluate(() => {
+      let translationData = document.querySelector('.VIiyi').innerText.split('\n')
+      return translationData
+    })
+    console.log(data)
+    let output = {}
+    Object.keys(JSON.parse(jsonString)).forEach((key, i) => {
+      output[key] = data[i]
+    })
+    console.log(output)
+    await delay(1000)
+  }
 
-  await page.screenshot({ path: './screenshots/example.png' }); 
-
+  // await page.screenshot({ path: './screenshots/example.png' }); 
   await browser.close(); 
 })();
